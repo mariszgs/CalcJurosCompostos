67ef24aac143a5ffde0f4ef4a635576888baa5d3
Mariana <mariansouza644@gmail.com>
2026-06-14 05:25:13 -0300

    primeiro commit

---
+++ src/main.py
@@ -0,0 +1,10 @@
+import flet as ft
+
+def main(page: ft.Page):
+    page.title = "Calculadora de Juros Compostos"
+    result = ft.Text(value="0")
+
+    
+    page.add(ft.Text(value="Mari Jane na Escuta Câmbio Desligo !!"))
+
+ft.run(main)
