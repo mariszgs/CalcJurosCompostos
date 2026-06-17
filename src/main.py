7ecf43adbe6954bea76c7d86d77c47f915063b6b
Mariana <mariansouza644@gmail.com>
2026-06-17 10:18:56 -0300

    feat:adicionando campos

--- src/main.py
+++ src/main.py
@@ -4,7 +4,28 @@ def main(page: ft.Page):
     page.title = "Calculadora de Juros Compostos"
     result = ft.Text(value="0")
 
+    page.add(
+        ft.Column(
+            controls=[
+            ft.Text(value="Simulador de Juros Compostos"),
+            ft.Row(
+            controls=[
+                ft.Text(value="Valor Inicial:"),
+                ft.TextField(width=150),
+                ft.Text(value="Valor Mensal:"),
+                ft.TextField(width=150),
+             ]
+          ),
+          ft.Row(
+              controls=[
+                  ft.Text(value="Taxa de Juros (%):"),
+                  ft.TextField(width=150),
+                  ft.Text(value="Período (meses):"),
+                  ft.TextField(width=150),
+              ]
+          )
+       ]
+    )
+)
     
-    page.add(ft.Text(value="Mari Jane na Escuta Câmbio Desligo !!"))
-
 ft.run(main)
