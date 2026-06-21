5788c7f468612746d9e881725107ae2f93fd79d2
Mariana <mariansouza644@gmail.com>
2026-06-20 22:43:22 -0300

    feat:adicionando botões e melhorando interface

--- src/main.py
+++ src/main.py
@@ -2,30 +2,120 @@ import flet as ft
 
 def main(page: ft.Page):
     page.title = "Calculadora de Juros Compostos"
-    result = ft.Text(value="0")
+
+    valor_inicial = ft.TextField(
+        width=340
+    )
+    valor_mensal = ft.TextField(
+        width=340
+    )
+    taxa = ft.TextField(
+        width=180
+    )
+    periodo = ft.TextField(
+        width=170
+    )
+
+    tipo_taxa = ft.Dropdown(
+        width=160,
+        value="anual",
+        options=[
+            ft.DropdownOption("mensal"),
+            ft.DropdownOption("anual"),
+        ]
+    )
+
+    tipo_periodo = ft.Dropdown(
+        width=160,
+        value="ano(s)",
+        options=[
+            ft.DropdownOption("mes(es)"),
+            ft.DropdownOption("ano(s)"),
+        ]
+    )
 
     page.add(
-        ft.Column(
+        ft.Row(
+            alignment=ft.MainAxisAlignment.CENTER,
+        controls=[
+            ft.Container(
+                width=900,
+                padding=30,
+                bgcolor="#DCDCDC",
+                border_radius=10,
+                
+        content=ft.Column(
             controls=[
-            ft.Text(value="Simulador de Juros Compostos"),
+            ft.Text(value="Simulador de Juros Compostos",
+            size=25,
+            weight=ft.FontWeight.BOLD,
+            color="#000080"
+        ),
+
             ft.Row(
+                alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
+                controls=[
+                    ft.Column(
+                        controls=[
+                            ft.Text("Valor Inicial"),
+                            valor_inicial,
+                        ]
+                    ),
+                    ft.Column(
+                        controls=[
+                            ft.Text("Valor Mensal:"),
+                            valor_mensal,
+                        ]
+                    )
+                ]
+            ),
+
+            ft.Row(
+            alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
             controls=[
-                ft.Text(value="Valor Inicial:"),
-                ft.TextField(width=150),
-                ft.Text(value="Valor Mensal:"),
-                ft.TextField(width=150),
+                ft.Column(
+                    controls=[
+                        ft.Text("Taxa de Juros:"),
+                        ft.Row(
+                            controls=[
+                                taxa,
+                                tipo_taxa,
+                            ]
+                        )
+                    ]
+                ),
+                ft.Column(
+                    controls=[
+                        ft.Text("Período:"),
+                        ft.Row(
+                            controls=[
+                                periodo,
+                                tipo_periodo,
+                            ]
+                        )
+                    ]
+                )
              ]
           ),
+
           ft.Row(
+              alignment=ft.MainAxisAlignment.SPACE_BETWEEN,
               controls=[
-                  ft.Text(value="Taxa de Juros (%):"),
-                  ft.TextField(width=150),
-                  ft.Text(value="Período (meses):"),
-                  ft.TextField(width=150),
-              ]
-          )
-       ]
+                  ft.Button(
+                    content="Calcular",
+                    color="#000080"
+                    ),
+                  ft.Button(
+                    content="Limpar",
+                    color="#8B0000"
+                    ),
+                                ],
+                            ),
+                        ]
+                    ),
+                )
+            ],
+        )
     )
-)
     
 ft.run(main)
